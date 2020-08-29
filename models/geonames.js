0effa153dc92e96c64fa4b96855422a646d3b7da
Michael Peterson <michaeljoepeterson@gmail.com>
2020-08-29 16:00:34 -0600

    getting cities correctly back for bc and returning cleaned cities

--- models/geonames.js
+++ models/geonames.js
@@ -19,15 +19,66 @@ class GeonamesInterface{
         }
     }
 
-    getChildren = async(id) => {
+    cleanCities = (cities) => {
+        let filters = ['county of', 'improvement','municipal'];
+        let cleanedCities = cities.filter(city => {
+            let lowerCity = city.toLowerCase();
+            let hasFilter = filters.find(filter => lowerCity.includes(filter));
+            if(!hasFilter){
+                return true;
+            }
+            else{
+                return false;
+            }
+        });
+
+        return cleanedCities.sort();
+    }
+
+    getChildren = async(id,isBc) => {
         try{
-            id = id ? id : CANADA_ID;
-            let url = `${GEONAMES_URL}childrenJSON?geonameId=${id}&username=${GEONAMES_USER}`;
+
+                id = id ? id : CANADA_ID;
+                let url = `${GEONAMES_URL}childrenJSON?geonameId=${id}&username=${GEONAMES_USER}`;
+                let results = await axios.get(url);
+                let data = results.data;
+                let names = results.data.geonames && results.data.geonames.length > 0 ? data.geonames.map(child => child.name)
+                : null;
+                if(isBc){
+                    let allNames = [];
+                    //console.log(data.geonames);
+                    names = await Promise.all(data.geonames.map(async (district)=>{
+                        let id = district.geonameId;
+                        console.log('getting next cities: ');
+                        let cities = await this.getChildren(id);
+                        allNames = allNames.concat(cities);
+                        console.log(id,allNames);
+                    }));
+                    allNames = allNames.sort();
+                    allNames = allNames.filter((value, index, self) => self.indexOf(value) === index);
+                    allNames = this.cleanCities(allNames);
+                    return allNames
+                }
+                else{
+                    names = this.cleanCities(names);
+                    return names;
+                }
+                
+        }
+        catch(err){
+            throw err;
+        }
+    }
+
+    getProvince = async(name) => {
+        try{
+            let target = 'first-order administrative division';
+            let url = `${GEONAMES_URL}searchJSON?name=${name}&username=${GEONAMES_USER}`;
             let results = await axios.get(url);
             let data = results.data;
-            let names = results.data.geonames && results.data.geonames.length > 0 ? data.geonames.map(child => child.name)
-             : null;
-            return names;
+            let province = data.geonames && data.geonames.length > 0 ? data.geonames.find(result => result.fcodeName.trim() === target) : null;
+    
+            return province.geonameId;
         }
         catch(err){
             throw err;
--- routers/geonames-router.js
+++ routers/geonames-router.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const {GeonamesInterface} = require('../models/geonames');
+const NodeCache = require( "node-cache" );
+let timeOutDefault = (60 * 1000) * 15;
+let checkPeriod = (60 * 1000) * 20;
+const myCache = new NodeCache({ stdTTL: timeOutDefault, checkperiod: checkPeriod });
 
 router.get('/getId',async (req,res,next) => {
     let {name} = req.query;
+    let existingId = myCache.get(name);
+    if(existingId){
+        return res.json({
+            code:200,
+            message:'success existing',
+            id:existingId
+        });
+    }
     let geonames = new GeonamesInterface();
-    //check cache before
+    
     try{
         let id = await geonames.getId(name);
+        myCache.set( name, id );
         return res.json({
             code:200,
             message:'success',
@@ -24,9 +37,18 @@ router.get('/getId',async (req,res,next) => {
 //default route to get provinces
 router.get('/provinces',async (req,res,next) => {
     let geonames = new GeonamesInterface();
-    //check cache before
+    let provinceKey = 'provinces';
+    let existingProvinces = myCache.get(provinceKey);
+    if(existingProvinces){
+        return res.json({
+            code:200,
+            message:'success existing',
+            provinces:existingProvinces
+        });
+    }
     try{
         let provinces = await geonames.getChildren();
+        myCache.set( provinceKey, provinces );
         return res.json({
             code:200,
             message:'success',
@@ -44,10 +66,19 @@ router.get('/provinces',async (req,res,next) => {
 router.get('/cities',async (req,res,next) => {
     let {name} = req.query;
     let geonames = new GeonamesInterface();
-    //check cache before for existing id to speed up dropdowns
+    let existingCities = myCache.get(name);
+    if(existingCities){
+        return res.json({
+            code:200,
+            message:'success existing',
+            cities:existingCities
+        });
+    }
     try{
-        let id = await geonames.getId(name);
-        let cities = await geonames.getChildren(id);
+        let id = await geonames.getProvince(name);
+        let isBc = name.toLowerCase() === 'british columbia' ? true:false;
+        let cities = await geonames.getChildren(id,isBc);
+        myCache.set( name, cities );
         return res.json({
             code:200,
             message:'success',
