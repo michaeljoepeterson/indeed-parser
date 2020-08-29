60dbb8841eb0d326768f856103cab439eea4f128
Michael Peterson <michaeljoepeterson@gmail.com>
2020-08-29 11:14:56 -0600

    setup provinces endpoint

--- models/geonames.js
+++ models/geonames.js
@@ -1,18 +1,33 @@
 const axios = require('axios');
-const {GEONAMES_URL, GEONAMES_USER} = require('../config');
+const {GEONAMES_URL, GEONAMES_USER,CANADA_ID} = require('../config');
 
 class GeonamesInterface{
     constructor(){
 
     }
 
-    getId = async (name) => {
+    getId = async(name) => {
         try{
             let url = `${GEONAMES_URL}searchJSON?name=${name}&username=${GEONAMES_USER}`;
             let results = await axios.get(url);
-            console.log(results);
-            let id = results.data.geonames && results.data.geonames.length > 0 ? results.data.geonames[0].geonameId : null;
+            let data = results.data;
+            let id = data.geonames && data.geonames.length > 0 ? data.geonames[0].geonameId : null;
             return id;
+        }
+        catch(err){
+            throw err;
+        }
+    }
+
+    getChildren = async(id) => {
+        try{
+            id = id ? id : CANADA_ID;
+            let url = `${GEONAMES_URL}childrenJSON?geonameId=${id}&username=${GEONAMES_USER}`;
+            let results = await axios.get(url);
+            let data = results.data;
+            let names = results.data.geonames && results.data.geonames.length > 0 ? data.geonames.map(child => child.name)
+             : null;
+            return names;
         }
         catch(err){
             throw err;
--- routers/geonames-router.js
+++ routers/geonames-router.js
@@ -5,11 +5,33 @@ const {GeonamesInterface} = require('../models/geonames');
 router.get('/getId',async (req,res,next) => {
     let {name} = req.query;
     let geonames = new GeonamesInterface();
+    //check cache before
     try{
         let id = await geonames.getId(name);
         return res.json({
+            code:200,
             message:'success',
             id
+        });
+    }
+    catch(err){
+        console.log('error getting geoid',err);
+        next();
+    }
+
+    
+});
+//default route to get provinces
+router.get('/provinces',async (req,res,next) => {
+    let {name} = req.query;
+    let geonames = new GeonamesInterface();
+    //check cache before
+    try{
+        let provinces = await geonames.getChildren();
+        return res.json({
+            code:200,
+            message:'success',
+            provinces
         });
     }
     catch(err){
